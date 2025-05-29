aca17fc67a6965cd0281a44d8133d2105ce8493f
Wolfram Kriesing <w+codeberg@kriesing.de>
2025-05-29 20:50:43 +0200

    A bit of refactoring and introducing the files-stats.

--- src/plugins/stats.js
+++ src/plugins/stats.js
@@ -1,5 +1,6 @@
 /**
  * @typedef {{filename: Filename, dateISO: DateISO, prettyUrlPath: UrlPath}} LastModifiedStats
+ * @typedef {{numFiles: number, numFolders: number, numFilesWithFrontmatter: number}} FilesStats
  * @typedef {{lastModified: LastModifiedStats}} Stats
  */
 
@@ -11,24 +12,36 @@ const toDate = (file) => new Date(file[1]._file.lastModifiedISO)
 
 /**
  * @param files {FilesMap}
+ * @return {LastModifiedStats}
  */
-const addStatsProperty = (files) => {
+const lastModifiedStats = files => {
   let latest = files.entries().next().value;
   for (const entry of files) {
     if (toDate(latest) < toDate(entry)) {
       latest = entry;
     }
   }
-  /** @type {LastModifiedStats} */
-  const lastModified = {
+  return {
     filename: latest[1]._file.relativeFilePath,
     dateISO: latest[1]._file.lastModifiedISO,
     prettyUrlPath: latest[1]._output.prettyUrlPath
   };
+};
+
+const filesStats = () => {
   
+}
+
+/**
+ * @param files {FilesMap}
+ */
+const addStatsProperty = (files) => {
   // Add the stats to all files.
   for (const [_, data] of files) {
-    data._stats = {lastModified};
+    data._stats = {
+      lastModified: lastModifiedStats(files),
+      files: filesStats(files),
+    };
   }
 }
 
